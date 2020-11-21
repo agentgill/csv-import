a18cd9cb9aaf600677ce14c06450811beaf38fd2
Mike Gill <mike@cloudava.com>
2020-11-21 11:50:26 +0000

    Move csvWriter inside function

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const csv = require('csvtojson');
 const csvFilePath = 'data.csv';
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-// Transform (Mapping)
-const csvWriter = createCsvWriter({
-  path: 'out.csv',
-  header: [
-    { id: 'PartnerId', title: 'OtherRef__c' },
-    { id: 'InvoiceNumber', title: 'ExternalId__c' },
-    { id: 'ConsumedQuantity', title: 'Quantity__c' },
-  ],
-});
-
 async function writeMyCSV() {
+  // Transform (Mapping)
+  const csvWriter = createCsvWriter({
+    path: 'out.csv',
+    header: [
+      { id: 'PartnerId', title: 'OtherRef__c' },
+      { id: 'InvoiceNumber', title: 'ExternalId__c' },
+      { id: 'ConsumedQuantity', title: 'Quantity__c' },
+    ],
+ });
+  
   const jsonArray = await csv().fromFile(csvFilePath);
   const csvData = await csvWriter
     .writeRecords(jsonArray)
